4febcf7bdbab611fadc76fae12f8723df47adac6
Mohamad Sorkhi <mohamad.sorkhi@gmail.com>
2026-06-18 20:22:56 +0430

    chore: clean up storage view cache from git

--- storage/framework/views/08c67a5c490be2c02b5b302207a077b8.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH /home/engipi/laravel_app/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
--- storage/framework/views/9a045967c15cb7248686e9419bd5b553.php
+++
@@ -1,5 +0,0 @@
-
-<body>
-
-<!-- <body data-layout="horizontal"> -->
-<?php /**PATH /home/engipi/laravel_app/resources/views/layouts/body.blade.php ENDPATH**/ ?>
